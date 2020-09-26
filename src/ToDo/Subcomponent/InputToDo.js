85b3e205a617ca18317021b775e06449770d59f5
Meysam Shariatmadary <meysamshariatmadary@gmail.com>
2020-09-26 15:13:52 +0330

    add ToDo file and its subcomponents

---
+++ src/ToDo/Subcomponent/InputToDo.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {ToDoStyles} from "./styles/ToDoStyles";
+
+class InputToDo extends React.Component {
+   
+    render(){
+       
+        return(
+            <div style={ToDoStyles.inputToDoStyle} >
+                <div style={ToDoStyles.newToDo}>
+                <form>
+                    <input type="text" id="filter" placeholder="new todo" ref={input => this.search = input} onChange={this.handleInputChange}/>
+                </form>
+                </div>
+
+                <div style={ToDoStyles.markAsImportant}>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            category: 
+                            <select value={this.state.value} onChange={this.handleChange}>
+                                <option value="Work">Work</option>
+                                <option value="Hobby">Hobby</option>
+                                <option value="important">Important</option>
+                            </select>
+                        </label>
+                    {/* <input type="submit" value="Submit" style={ToDoStyles.filter}/>  */}
+                </form>
+                </div>
+            </div>
+            );
+    }
+}
+export default InputToDo;
---
+++ src/ToDo/ToDo.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ToDoHeader from "./Subcomponent/ToDoHeader";
+import ToDoList from "./Subcomponent/ToDoList";
+import ToDoFilter from "./Subcomponent/ToDoFilter";
+import InputToDo from "./Subcomponent/InputToDo";
+import { ToDoStyles } from "./Subcomponent/styles/ToDoStyles";
+
+class ToDo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newToDo: '',
+            category: ''
+        };
+    }
+    
+    render() {
+        return(
+            <div style={ToDoStyles.backGround}>
+                <div style={ToDoStyles.toDo}>
+                <ToDoHeader />
+                <div style={ToDoStyles.line}>
+    
+                </div>
+                <ToDoList />
+                <ToDoFilter />
+                <InputToDo />
+            </div>
+            </div>
+            );
+    }
+}
+export default ToDo;
